0af44c7daddf37f2183fe237c3b0b51462012eb4
Marti <villa12marti@gmail.com>
2026-05-20 12:56:11 -0300

    start game fixed
    there was a warning cause i was calling the start methods in update

--- Assets/Scripts/ServerController.cs
+++ Assets/Scripts/ServerController.cs
@@ -1,19 +1,35 @@
 using Unity.Netcode;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class ServerController : MonoBehaviour
 {
-    void Update()
-    {
-        if (Keyboard.current.hKey.wasPressedThisFrame)
-        {
-            NetworkManager.Singleton.StartHost(); //start host
-        }
+    [SerializeField] Button _createGame;
+    [SerializeField] Button _joinGame;
+    [SerializeField] GameObject _canvasStart;
 
-        if (Keyboard.current.cKey.wasPressedThisFrame)
-        {
-            NetworkManager.Singleton.StartClient(); //start client
-        }
+    void Start()
+    {
+        _createGame.onClick.AddListener(CreateGameClicked);
+        _joinGame.onClick.AddListener(JoinGameClicked);
+    }
+
+    void CreateGameClicked()
+    {
+        NetworkManager.Singleton.StartHost(); //start host
+        TurnCanvasOff();
+    }
+
+    void JoinGameClicked()
+    {
+        NetworkManager.Singleton.StartClient(); //start client
+        TurnCanvasOff();
+    }
+
+    void TurnCanvasOff()
+    {
+        _canvasStart.SetActive(false);
     }
 }
